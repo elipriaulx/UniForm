7ece4b6a418af5cb270311a782493e70a57396c7
gpriaulx <gui.priaulx@live.com.au>
2019-01-14 23:13:38 +1100

    Update Logos

--- Scripts/appveyor-postbuild.ps1
+++ Scripts/appveyor-postbuild.ps1
@@ -4,5 +4,6 @@ robocopy ".\UniForm.Engine\bin\Release\netstandard2.0" ".\UniForm.Engine\bin\Any
 # robocopy ".\UniForm.Core\bin\Release\netstandard2.0" "C:.\UniForm.Core\bin\Any CPU\Release\netstandard2.0" *.* /MIR
 
 Tree
+dir ".\UniForm.Wpf\bin\Release\"
 
 ./Scripts/pack.bat
