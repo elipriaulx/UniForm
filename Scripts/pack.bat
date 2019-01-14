60fac54956cfc4006a2daf8432fe8245de998d03
gpriaulx <gui.priaulx@live.com.au>
2019-01-14 15:44:45 +1100

    INcrease pack verbosity

--- Scripts/pack.bat
+++ Scripts/pack.bat
@@ -1,4 +1,4 @@
-.\Tools\nuget.exe pack UniForm.Core\UniForm.Core.csproj -Prop Configuration=Release
-.\Tools\nuget.exe pack UniForm.Engine\UniForm.Engine.csproj -Prop Configuration=Release
-.\Tools\nuget.exe pack UniForm.Wpf\UniForm.Wpf.csproj -Prop Configuration=Release
+.\Tools\nuget.exe pack UniForm.Core\UniForm.Core.csproj -Prop Configuration=Release -Verbosity detailed
+.\Tools\nuget.exe pack UniForm.Engine\UniForm.Engine.csproj -Prop Configuration=Release -Verbosity detailed
+.\Tools\nuget.exe pack UniForm.Wpf\UniForm.Wpf.csproj -Prop Configuration=Release -Verbosity detailed
 
