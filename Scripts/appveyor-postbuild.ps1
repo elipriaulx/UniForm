3cb35bb45171c2589c9c664dd553982aa7cf9f02
gpriaulx <gui.priaulx@live.com.au>
2019-01-14 23:27:23 +1100

    Fix package build

--- Scripts/appveyor-postbuild.ps1
+++ Scripts/appveyor-postbuild.ps1
@@ -1,9 +1,4 @@
 robocopy ".\UniForm.Core\bin\Release\netstandard2.0" ".\UniForm.Core\bin\Any CPU\Release\netstandard2.0" *.* /MIR
 robocopy ".\UniForm.Engine\bin\Release\netstandard2.0" ".\UniForm.Engine\bin\Any CPU\Release\netstandard2.0" *.* /MIR
 
-# robocopy ".\UniForm.Core\bin\Release\netstandard2.0" "C:.\UniForm.Core\bin\Any CPU\Release\netstandard2.0" *.* /MIR
-
-Tree
-dir ".\UniForm.Wpf\bin\Release\"
-
 ./Scripts/pack.bat
